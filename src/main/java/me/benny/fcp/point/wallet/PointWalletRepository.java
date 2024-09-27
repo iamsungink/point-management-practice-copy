5a9dcbb7713f3f2a5f2f55091ba55fade751f042
iamsungink <iamsungink@gmail.com>
2024-09-28 07:25:19 +0900

    #4-biz-setting: Entity 구성

---
+++ src/main/java/me/benny/fcp/point/wallet/PointWalletRepository.java
@@ -0,0 +1,6 @@
+package me.benny.fcp.point.wallet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointWalletRepository extends JpaRepository<PointWallet, Long> {
+}
